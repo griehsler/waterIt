77faa24a53814817c9aa51f41d00714a627a7dac
Ronald Griehsler <ronald.griehsler@gmail.com>
2018-12-08 00:39:09 +0100

    deactivated debugger mode

--- server/dataportal.py
+++ server/dataportal.py
@@ -70,4 +70,4 @@ class WebService(object):
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
     app = WebService()
-    run_simple('0.0.0.0', 5000, app, use_debugger=True, use_reloader=True)
+    run_simple('0.0.0.0', 5000, app, use_debugger=False, use_reloader=False)
